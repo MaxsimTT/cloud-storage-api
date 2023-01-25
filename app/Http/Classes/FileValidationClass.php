cc48784b393de6c10cb20a283600cda22d5bd638
MaxsimTT <egorovcotmax@gmail.com>
2023-01-25 20:55:23 +0400

    updated validate

--- app/Http/Classes/FileValidationClass.php
+++ app/Http/Classes/FileValidationClass.php
@@ -10,27 +10,39 @@ class FileValidationClass
 	public static function validation(array $files)
 	{
 
-		self::arr($files);
+		$res = [];
 
-		// foreach ($files as $file) {
-		// 	if ($file['error']) {
-		// 		continue;
-		// 	}
+		foreach (self::arr($files) as $file) {
 
-		// 	self::$files[] = $file;
-		// }
+			if ($file['error']) {
+				continue;
+			}
 
+			if (strtolower(substr(strrchr($file['name'], '.'), 1)) === 'php') {
+				continue;
+			}
+
+			$res[] = $file;
+		}
+
+		dump($res);
 		// return self::$files;
 	}
 
-	private static function arr(array $files)
+	private static function arr(array $files): array
 	{
 		$res_files = [];
 
 		$files_count = count($files['name']);
+		$file_attrs = array_keys($files);
 
-		dd($files_count);
-		return $files;
+		for ($i = 0; $i < $files_count; $i++) {
+			foreach ($file_attrs as $file_attr) {
+				$res_files[$i][$file_attr] = $files[$file_attr][$i];
+			}
+		}
+
+		return $res_files;
 	}
 
 }
