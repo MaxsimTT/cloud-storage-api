58d89ef8f4fbc2be44f9ed3f53d5460caa7310be
Max <egorovcotmax@gmail.com>
2023-01-30 15:19:33 +0400

    added sidebar layout

--- resources/views/files.blade.php
+++ resources/views/files.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
 
     <div class="row justify-content-center">
-        <div class="col-md-10">
+        <div class="col-md-12">
 
 			@if (session('message'))
 			    <div class="alert alert-success">
